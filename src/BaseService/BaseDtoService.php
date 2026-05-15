67049a5d837de1c0721a63081665f86d8a7829a6
denmarty-dev <denisarshavin98@gmail.com>
2026-05-15 10:24:24 +0100

    Update BaseDtoService.php

--- src/BaseService/BaseDtoService.php
+++ src/BaseService/BaseDtoService.php
@@ -1,223 +1,223 @@
 <?php
 
-namespace Denmarty\BaseServiceRepository\BaseService;
+  namespace Denmarty\BaseServiceRepository\BaseService;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Spatie\LaravelData\Data;
+  use Illuminate\Database\Eloquent\Builder;
+  use Illuminate\Database\Eloquent\Collection;
+  use Illuminate\Database\Eloquent\Model;
+  use Spatie\LaravelData\Data;
 
-/**
- *
- */
-abstract class BaseDtoService
-{
-    /**
-     * @var BaseRepository
-     */
-    public BaseRepositoryInterface $baseRepository;
+  /**
+   * @template TRepo of BaseRepositoryInterface
+   * @template TDto of Data
+   */
+  abstract class BaseDtoService
+  {
+      /** @var TRepo */
+      public BaseRepositoryInterface $baseRepository;
 
-    public Data $dto;
+      /** @var TDto */
+      public Data $dto;
 
-    /**
-     * @param array $attr
-     * @return Builder
-     */
-    public function index(array $attr = []): Builder
-    {
-        return $this->baseRepository->index($attr);
-    }
+      /**
+       * @param array $attr
+       * @return Builder
+       */
+      public function index(array $attr = []): Builder
+      {
+          return $this->baseRepository->index($attr);
+      }
 
-    /**
-     * @param $id
-     * @return Model|Collection|Builder|array|null
-     */
-    public function getById($id, $isDto = true): Model|Collection|Builder|array|null|Data
-    {
-        $data = $this->baseRepository->getById($id);
+      /**
+       * @param $id
+       * @return Model|Collection|Builder|array|null|Data
+       */
+      public function getById($id, $isDto = true): Model|Collection|Builder|array|null|Data
+      {
+          $data = $this->baseRepository->getById($id);
 
-        if ($isDto) {
-            $data = $this->dto::from($data);
-        }
+          if ($isDto) {
+              $data = $this->dto::from($data);
+          }
 
-        return $data;
-    }
+          return $data;
+      }
 
-    /**
-     * @param array $data
-     * @return Model|Builder
-     */
-    public function create(array $data, $isDto = true): Model|Builder|Data
-    {
-        $result = $this->baseRepository->create($data);
+      /**
+       * @param array $data
+       * @return Model|Builder|Data
+       */
+      public function create(array $data, $isDto = true): Model|Builder|Data
+      {
+          $result = $this->baseRepository->create($data);
 
-        if ($isDto) {
-            $result = $this->dto::from($result);
-        }
+          if ($isDto) {
+              $result = $this->dto::from($result);
+          }
 
-        return $result;
-    }
+          return $result;
+      }
 
-    /**
-     * @param $id
-     * @param array $data
-     * @return bool
-     */
-    public function update($id, array $data): bool
-    {
-        return $this->baseRepository->update($id, $data);
-    }
+      /**
+       * @param $id
+       * @param array $data
+       * @return bool
+       */
+      public function update($id, array $data): bool
+      {
+          return $this->baseRepository->update($id, $data);
+      }
 
-    /**
-     * @param string $uuid
-     * @param array $data
-     * @return Model|null
-     */
-    public function updateByUUID(string $uuid, array $data, $isDto = true): Model|null|Data
-    {
-        $result = $this->baseRepository->updateByUUID($uuid, $data);
+      /**
+       * @param string $uuid
+       * @param array $data
+       * @return Model|null|Data
+       */
+      public function updateByUUID(string $uuid, array $data, $isDto = true): Model|null|Data
+      {
+          $result = $this->baseRepository->updateByUUID($uuid, $data);
 
-        if ($isDto) {
-            $result = $this->dto::from($result);
-        }
+          if ($isDto) {
+              $result = $this->dto::from($result);
+          }
 
-        return $result;
-    }
+          return $result;
+      }
 
-    /**
-     * @param int $id
-     * @param array $data
-     * @return Model|null
-     */
-    public function updateById(int $id, array $data, $isDto = true): Model|null|Data
-    {
-        $result = $this->baseRepository->updateById($id, $data);
+      /**
+       * @param int $id
+       * @param array $data
+       * @return Model|null|Data
+       */
+      public function updateById(int $id, array $data, $isDto = true): Model|null|Data
+      {
+          $result = $this->baseRepository->updateById($id, $data);
 
-        if ($isDto) {
-            $result = $this->dto::from($result);
-        }
+          if ($isDto) {
+              $result = $this->dto::from($result);
+          }
 
-        return $result;
-    }
+          return $result;
+      }
 
-    /**
-     * @param array $attr
-     * @param array $data
-     * @return Model|Builder
-     */
-    public function updateOrCreate(array $attr, array $data, $isDto = true): Model|Builder|Data
-    {
-        $result = $this->baseRepository->updateOrCreate($attr, $data);
+      /**
+       * @param array $attr
+       * @param array $data
+       * @return Model|Builder|Data
+       */
+      public function updateOrCreate(array $attr, array $data, $isDto = true): Model|Builder|Data
+      {
+          $result = $this->baseRepository->updateOrCreate($attr, $data);
 
-        if ($isDto) {
-            $result = $this->dto::from($result);
-        }
+          if ($isDto) {
+              $result = $this->dto::from($result);
+          }
 
-        return $result;
-    }
+          return $result;
+      }
 
-    /**
-     * Create or update a record matching the attributes, and fill it with values.
-     *
-     * @param array $data
-     * @return int
-     */
-    public function insert(array $data): int
-    {
-        return $this->baseRepository->insert($data);
-    }
+      /**
+       * Create or update a record matching the attributes, and fill it with values.
+       *
+       * @param array $data
+       * @return int
+       */
+      public function insert(array $data): int
+      {
+          return $this->baseRepository->insert($data);
+      }
 
-    /**
-     * Create or update a record matching the attributes, and fill it with values.
-     *
-     * @param array $data
-     * @param array $uniqueFields
-     * @param $updatedFields
-     * @return int
-     */
-    public function upsert(array $data, array $uniqueFields, $updatedFields): int
-    {
-        return $this->baseRepository->upsert($data, $uniqueFields, $updatedFields);
-    }
+      /**
+       * Create or update a record matching the attributes, and fill it with values.
+       *
+       * @param array $data
+       * @param array $uniqueFields
+       * @param $updatedFields
+       * @return int
+       */
+      public function upsert(array $data, array $uniqueFields, $updatedFields): int
+      {
+          return $this->baseRepository->upsert($data, $uniqueFields, $updatedFields);
+      }
 
-    /**
-     * @param $id
-     * @return bool
-     */
-    public function deleteById($id): bool
-    {
-        return $this->baseRepository->deleteById($id);
-    }
+      /**
+       * @param $id
+       * @return bool
+       */
+      public function deleteById($id): bool
+      {
+          return $this->baseRepository->deleteById($id);
+      }
 
+      /**
+       * @param string $uuid
+       * @return Model|Collection|Builder|array|null|Data
+       */
+      public function getByUuid(string $uuid, $isDto = true): Model|Collection|Builder|array|null|Data
+      {
+          $result = $this->baseRepository->getByUuid($uuid);
 
-    /**
-     * @param string $uuid
-     * @return Model|Collection|Builder|array|null
-     */
-    public function getByUuid(string $uuid, $isDto = true): Model|Collection|Builder|array|null|Data
-    {
-        $result = $this->baseRepository->getByUuid($uuid);
+          if ($isDto) {
+              $result = $this->dto::from($result);
+          }
 
-        if ($isDto) {
-            $result = $this->dto::from($result);
-        }
+          return $result;
+      }
 
-        return $result;
-    }
+      /**
+       * @param string $uuid
+       * @return bool
+       */
+      public function deleteByUuid(string $uuid): bool
+      {
+          return $this->baseRepository->deleteByUuid($uuid);
+      }
 
-    /**
-     * @param string $uuid
-     * @return bool
-     */
-    public function deleteByUuid(string $uuid): bool
-    {
-        return $this->baseRepository->deleteByUuid($uuid);
-    }
+      /**
+       * Получаем строку из бд по названию колонки и значению
+       *
+       * @param mixed $value
+       * @param string $columnName
+       * @param array|null $relations
+       * @param string $operator
+       * @return Model|null|Data
+       */
+      public function getRowByColumn(
+          mixed $value,
+          string $columnName = 'id',
+          ?array $relations = null,
+          string $operator = '=',
+          $isDto = true
+      ): null|Model|Data {
+          $result = $this->index()
+              ->where($columnName, $operator, $value)
+              ->when($relations, function ($query) use ($relations) {
+                  return $query->with($relations);
+              })
+              ->first();
 
-    /**
-     * Получаем строку из бд по названию колонки и значению
-     * @param mixed $value
-     * @param string $columnName
-     * @param array|null $relations
-     * @param string $operator
-     * @return Model|null
-     */
-    public function getRowByColumn(
-        mixed $value,
-        string $columnName = 'id',
-        ?array $relations = null,
-        string $operator = '=',
-        $isDto = true
-    ): null|Model|Data {
-        $result = $this->index()
-            ->where($columnName, $operator, $value)
-            ->when($relations, function ($query) use ($relations) {
-                return $query->with($relations);
-            })
-            ->first();
+          if ($isDto) {
+              $result = $this->dto::from($result);
+          }
 
-        if ($isDto) {
-            $result = $this->dto::from($result);
-        }
+          return $result;
+      }
 
-        return $result;
-    }
+      /**
+       * @param int $id
+       * @return string|null
+       */
+      public function getUuidById(int $id): string|null
+      {
+          return $this->baseRepository->getUuidById($id);
+      }
 
-    /**
-     * @param int $id
-     * @return string|null
-     */
-    public function getUuidById(int $id): string|null
-    {
-        return $this->baseRepository->getUuidById($id);
-    }
-
-    /**
-     * @param string $uuid
-     * @return int|null
-     */
-    public function getIdByUuid(string $uuid): int|null
-    {
-        return $this->baseRepository->getIdByUuid($uuid);
-    }
-}
+      /**
+       * @param string $uuid
+       * @return int|null
+       */
+      public function getIdByUuid(string $uuid): int|null
+      {
+          return $this->baseRepository->getIdByUuid($uuid);
+      }
+  }
